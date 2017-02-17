5840078a9e9d6bd3cbfb7174d41e449d91ae883e
Knut Luescher <klue@ch.ibm.com>
2017-02-17 15:56:14 +0100

    EJS examples added

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,10 @@
  */
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+  //res.send("respond with a resource");
+	var list = [
+		{name: '1', value: "one"},
+		{name: '2', value: "two"},
+	]
+	res.render('pages/simple', { title: 'Simple EJS example' , elements: list});
 };
